3b2681cae84603c7642ede2ead277467afb55945
Mike Darwish <mike@learningbird.com>
2016-07-27 17:36:08 +0000

    Added documentation

--- src/LbConf/Config/ConfigManager.php
+++ src/LbConf/Config/ConfigManager.php
@@ -5,6 +5,27 @@ use Exceptions\Collection\KeyNotFoundException;
 use Exceptions\Data\FormatException;
 use Exceptions\IO\Filesystem\FileNotFoundException;
 
+/**
+ * Class ConfigManager
+ *
+ * Manages hierarchical JSON configuration files.
+ *
+ * Files to read/write are specified in a meta-configuration file that must be passed to ConfigManager::loadConfig().
+ * @see loadConfig() for the meta-configuration file structure.
+ *
+ * Example usage:
+ *     $configManager = new ConfigManager();
+ *     $configManager->loadConfig('/var/www/.lbconf');
+ *
+ *     $configManager->keys(); // [ 'database', 'services', 'debug', ... ]
+ *
+ *     $configManager->get('database.username'); // 'prod-user'
+ *
+ *     $configManager->set('database.username', 'dev-user')
+ *     $configManager->storeConfig();
+ *
+ * @package LbConf\Config
+ */
 class ConfigManager
 {
     const TYPE_STRING  = 'string';
@@ -36,6 +57,24 @@ class ConfigManager
         $this->configMerger = new ConfigMerger();
     }
 
+    /**
+     * Load configuration from a meta-config file.
+     * The meta-config file must be in JSON format with the following structure:
+     * {
+     *     "read": [
+     *         "configurations/default.json",
+     *         "configurations/env-development.json"
+     *     ],
+     *     "write": "configurations/override.json"
+     * }
+     *
+     * File paths are relative to the location of the meta-config file.
+     *
+     * The files in BOTH the "read" and "write" sections are read and merged, and the file in the write section is
+     * written to when storeConfig is called.
+     *
+     * @param string $metaConfigFile
+     */
     public function loadConfig(string $metaConfigFile)
     {
         $metaConfig = $this->readConfigFile($metaConfigFile);
@@ -70,16 +109,37 @@ class ConfigManager
         $this->writeData = $this->readConfigFile($writeFile);
     }
 
+    /**
+     * Write any changes made by set() or delete() to the write file.
+     */
     public function storeConfig()
     {
         $this->writeConfigFile($this->writeFile, $this->writeData);
     }
 
+    /**
+     * Get data that will be written to the write file.
+     * This includes the original contents of the write file, plus any changes made by calls to set() and delete().
+     *
+     * @return array
+     */
     public function getWriteData(): array
     {
         return $this->writeData;
     }
 
+    /**
+     * Retrieve the configuration value for the specified key. If $key is not provided, all configuration data will be
+     * returned.
+     *
+     * Example:
+     *     $configManager->get('database'); // [ 'host' => 'localhost', 'port' => 3306, 'username' => 'prod-user', ... ]
+     *     $configManager->get('database.hostname'); // 'localhost'
+     *
+     * @param string|null $key
+     *
+     * @return mixed
+     */
     public function get(string $key = null)
     {
         if ($key === null) {
@@ -100,6 +160,19 @@ class ConfigManager
         return $element;
     }
 
+    /**
+     * Sets the configuration value for the specified key.
+     * The type of $value will be inferred, unless $type is explicitly set.
+     *
+     * Example:
+     *     $configManager->set('database.hostname', 'localhost');
+     *     $configManager->set('database.port', '3306'); // Will be cast to int
+     *     $configManager->set('database.port', '3306', 'string'); // Will be set as string
+     *
+     * @param string      $key
+     * @param mixed       $value
+     * @param string|null $type  If supplied, on of: string, number, boolean.
+     */
     public function set(string $key, $value, string $type = null)
     {
         $castValue = $this->cast($value, $type);
@@ -122,6 +195,13 @@ class ConfigManager
         $this->readData = $this->configMerger->merge($this->readData, $this->writeData);
     }
 
+    /**
+     * Deletes the configuration value for the specified key.
+     * Note that the key must exist in the write data for the deletion to be permitted. There is no way to delete a key
+     * that only exists in the read data, the only alternative is to set it to null, or some such value.
+     *
+     * @param string $key
+     */
     public function delete(string $key)
     {
         $pieces    = explode('.', $key);
@@ -139,6 +219,14 @@ class ConfigManager
         unset($element[$lastPiece]);
     }
 
+    /**
+     * Retrieves the keys for the configuration value under the specified key. If $key is not provided, all top-level
+     * keys will be returned.
+     *
+     * @param string|null $key
+     *
+     * @return array
+     */
     public function keys(string $key = null): array
     {
         $value = $this->get($key);
@@ -150,6 +238,14 @@ class ConfigManager
         return array_keys($value);
     }
 
+    /**
+     * Cast the given value to the specified type. If type is omitted, it will be inferred based on the value.
+     *
+     * @param             $value
+     * @param string|null $type
+     *
+     * @return bool|float|int|null|string
+     */
     protected function cast($value, string $type = null)
     {
         if ($type === null) {
@@ -174,6 +270,17 @@ class ConfigManager
         }
     }
 
+    /**
+     * Cast the given value to a type inferred based on the value.
+     *
+     * The strings 'true', 'false' and 'null' will be cast to true, false and null, respectively.
+     * Numeric values will be cast to integers or floats, depending on the presence of a decimal.
+     * Everything else will be returned as-is.
+     *
+     * @param $value
+     *
+     * @return bool|float|int|null|string
+     */
     protected function inferType($value)
     {
         if (strcasecmp($value, 'true') === 0) {
--- src/LbConf/Config/ConfigMerger.php
+++ src/LbConf/Config/ConfigMerger.php
@@ -3,7 +3,22 @@ namespace LbConf\Config;
 
 class ConfigMerger
 {
-    public function merge($target, $source)
+    /**
+     * Merges two arrays of configurations.
+     *
+     * The steps are as follows:
+     *     1. Start with the data in the target.
+     *     2. If a key exists only in the source, copy it to the target.
+     *     3. Otherwise, if either the target or source value is not an array, overwrite the target with the source.
+     *     4. Otherwise, if either the target or source array is non-associative, overwrite the target with the source.
+     *     5. Otherwise, recursively merge each element in the target and source values.
+     *
+     * @param $target
+     * @param $source
+     *
+     * @return array
+     */
+    public function merge($target, $source): array
     {
         foreach ($source as $key => $sourceValue) {
             if (array_key_exists($key, $target)) {
